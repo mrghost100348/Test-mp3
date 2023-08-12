f4b3c52ecadcfe4f0497e4ac95a3bc11a237eb02
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-12 21:51:03 +0530

    Update and rename owner-restart.js to tools-videotomsg

--- plugins/owner-restart.js
+++
@@ -1,7 +0,0 @@
-if (command == 'gn') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/main/src/Abhi-Json/random-ff`)).data;
-    const res2 = await res[Math.floor(res.length * Math.random())];
-    await conn.reply(m.chat, global.wait, m);
-    conn.sendMessage{image: {url: res2}});
-  }
-};
