04b5c9c6cea02bfd596043bc4e925ce861a1856b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-12 21:54:55 +0530

    Update tools-videotomsg.js

--- plugins/tools-videotomsg.js
+++ plugins/tools-videotomsg.js
@@ -1,16 +1,3 @@
-const {Module} = require('../main');
-Module({
-    pattern: 'ptv ?(.*)',
-    fromMe: true,
-    desc: "Whatsapp videoMessage feature",
-    use: 'whatsapp'
-}, async (m,match) => {
-if(m.reply_message.video) {
-const x = JSON.stringify(m.quoted).replace("videoMessage","ptvMessage"),jid = match[1] || m.jid
-m.quoted = JSON.parse(x)
-m.forwardMessage(jid,m.quoted,{contextInfo:{isForwarded:false}})
-} else { m.send("_Reply to a video_") }
-});
 Module({
     pattern: 'vdo ?(.*)',
     fromMe: true,
