e500d08ce94a1dbb11f9af8118cfd09ec732753a
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-13 00:58:17 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -9,7 +9,7 @@ handler.all = async function (m) {
 
   if (/^.menu$/i.test(m.text) ) {
     let av = '../src/mp3/menu.mp3'
-    this.sendPresenceUpdate('ABHISHEK-SER', m.chat);
+    this.sendPresenceUpdate('recording', m.chat);
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
