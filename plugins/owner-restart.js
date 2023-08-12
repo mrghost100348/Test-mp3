0c4ac990fd15ece141c29b30bea1aa7462c54a7d
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-12 20:28:22 +0530

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,7 +1,7 @@
 if (command == 'gn') {
     const res = (await axios.get(`https://raw.githubusercontent.com/AbhishekSuresh2/ABHISHEK-SER/main/src/Abhi-Json/random-ff`)).data;
     const res2 = await res[Math.floor(res.length * Math.random())];
-    await conn.reply(m.chat, Good Night, m);
-    conn.sendMessage(m.chat, res2 );
+    await conn.reply(m.chat, global.wait, m);
+    conn.sendMessage{image: {url: res2}});
   }
 };
