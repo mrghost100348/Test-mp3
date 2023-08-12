68bfd6904612beff68161e78c74485c2dabe917e
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-08-12 16:29:56 +0530

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, isROwner, text }) => {
     if (conn.user.jid == conn.user.jid) {
     await m.reply('🔄 Restarting Bot...\n Wait A Moment')
     process.send('reset')
+    await m.reply('_Restarted✅_')
   } else throw 'eh'
 }
 
