e554eac57ca1e555fe6e98bb063c2e3d0388a62f
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-09-11 12:37:49 +0530

    Update welgc_byegc.js

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn, text, usedPrefix, command, customPrefix }) => {
 let stikerwelgc = "./src/welgc.webp"
 let stikerbyegc = "./src/byegc.webp"
-if (command == 'welcomegc') {
+if (command == 'bot') {
 conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
 contextInfo: { externalAdReply: { title: ''𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯', body: '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️', sourceUrl: `https://github.com/AbhishekSuresh2/ABHISHEK-SER`, thumbnail: imagen1}}})  
 }
-if (command == 'byegc') {
+if (command == '.git') {
 conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
 contextInfo: { externalAdReply: { title: ''𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯', body: '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️', sourceUrl: `https://github.com/AbhishekSuresh2/ABHISHEK-SER`, thumbnail: imagen1}}})
 }}
-handler.command = ['welcomegc', 'byegc']
+handler.command = ['bot', '.git']
 export default handler
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]}
